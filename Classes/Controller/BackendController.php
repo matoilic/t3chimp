26501ff164e74710c4b7a3055e952988a81f4f6a
Mato Ilic <info@matoilic.ch>
2012-05-11 08:52:47 +0200

    backend module

---
+++ Classes/Controller/BackendController.php
@@ -0,0 +1,7 @@
+<?php
+
+class Tx_T3chimp_Controller_BackendController extends Tx_Extbase_MVC_Controller_ActionController {
+    public function indexAction() {
+
+    }
+}
--- ext_tables.php
+++ ext_tables.php
@@ -13,3 +13,20 @@ t3lib_extMgm::addPiFlexFormValue($_EXTKEY . '_subscription', 'FILE:EXT:t3chimp/C
 
 t3lib_extMgm::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'T3Chimp Setup');
 t3lib_extMgm::addStaticFile($_EXTKEY, 'Configuration/TypoScript/DefaultStyles', 'T3Chimp CSS Styles (optional)');
+
+if (TYPO3_MODE === 'BE') {
+    Tx_Extbase_Utility_Extension::registerModule(
+        $_EXTKEY,
+        'tools',
+        't3chimp',
+        '',
+        array(
+            'Backend' 	=> 'index'
+        ),
+        array(
+            'access' => 'user,group',
+            'icon'   => 'EXT:' . $_EXTKEY . '/ext_icon.png',
+            'labels' => 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_backend.xml',
+        )
+    );
+}
