99850c6cebb06355fdbbe2a101752c3230cc22ea
Mato Ilic <info@matoilic.ch>
2012-10-20 17:07:39 +0200

    fix compatibility with sr_feuser_register

--- Classes/Provider/Session.php
+++ Classes/Provider/Session.php
@@ -30,28 +30,16 @@ class Tx_T3chimp_Provider_Session implements t3lib_Singleton {
     const KEY = 'tx_t3chimp';
 
     /**
-     * @var tslib_feUserAuth
-     */
-    private $feUser;
-
-    /**
      * @var array
      */
     private $sessionData = array();
 
     public function __construct() {
-        //hold a reference to the fe_user object so the destructor has guaranteed access to it
-        $this->feUser = $GLOBALS['TSFE']->fe_user;
         $data = $this->feUser->getKey('ses', self::KEY);
 
         if($data != null) {
             $this->sessionData = unserialize($data);
         }
-    }
-
-    public function __destruct() {
-        $this->feUser->setKey('ses', self::KEY, serialize($this->sessionData));
-        $this->feUser->storeSessionData();
     }
 
     public function __get($key) {
@@ -64,6 +52,8 @@ class Tx_T3chimp_Provider_Session implements t3lib_Singleton {
 
     public function __set($key, $value) {
         $this->sessionData[$key] = $value;
+        $this->feUser->setKey('ses', self::KEY, serialize($this->sessionData));
+        $this->feUser->storeSessionData();
     }
 
     public function __unset($key) {
