724bfa390cb9ea6e378a12efee4a2c7f07c7b9a4
Mato Ilic <info@matoilic.ch>
2012-12-18 15:10:28 +0100

    v0.4.6

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,14 +3,14 @@ if (!defined ('TYPO3_MODE')) die ('Access denied.');
 
 $TYPO3_CONF_VARS['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['clearCachePostProc'][$_EXTKEY] = t3lib_extMgm::extPath($_EXTKEY).'Classes/Hook/Cache.php:&Tx_T3chimp_Hook_Cache->clearCache';
 
-if (TYPO3_MODE === 'BE' && TYPO3_version >= '4.6.0') {
+/*if (TYPO3_MODE === 'BE' && TYPO3_version >= '4.6.0') {
     $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['extbase']['commandControllers'][] = 'Tx_T3chimp_Command_SyncFeUsersCommandController';
     $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['extbase']['commandControllers'][] = 'Tx_T3chimp_Command_MaintenanceCommandController';
 
     if (TYPO3_version < '6.0.0') {
         $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['extbase']['commandControllers'][] = 'Tx_T3chimp_Command_MaintenanceCommandController';
     }
-}
+}*/
 
 Tx_Extbase_Utility_Extension::configurePlugin(
     $_EXTKEY,
