09f347eb32be1f2b6a7ef74e8f19b4b41073dc75
Mato Ilic <info@matoilic.ch>
2013-03-15 23:09:44 +0100

    copyright notice update

--- Classes/MailChimp/Field/PatternBased.php
+++ Classes/MailChimp/Field/PatternBased.php
@@ -3,7 +3,7 @@
 /***************************************************************
  *  Copyright notice
  *
- *  (c) 2012 Mato Ilic <info@matoilic.ch>
+ *  (c) 2013 Mato Ilic <info@matoilic.ch>
  *  All rights reserved
  *
  *  This script is part of the TYPO3 project. The TYPO3 project is
