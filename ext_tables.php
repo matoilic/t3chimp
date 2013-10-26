7e4d0d281a0ab55a3571ac380e7ce551399ce56e
Mato Ilic <info@matoilic.ch>
2013-10-26 16:15:13 +0200

    disable the backend module since mailchimp doesn't seem to allow to load the page within an iframe

--- ext_tables.php
+++ ext_tables.php
@@ -9,7 +9,7 @@ t3lib_extMgm::addPiFlexFormValue($_EXTKEY . '_subscription', 'FILE:EXT:t3chimp/C
 t3lib_extMgm::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'T3Chimp Setup');
 t3lib_extMgm::addStaticFile($_EXTKEY, 'Configuration/TypoScript/DefaultStyles', 'T3Chimp CSS Styles (optional)');
 
-if (TYPO3_MODE === 'BE') {
+/*if (TYPO3_MODE === 'BE') {
     Tx_Extbase_Utility_Extension::registerModule(
         $_EXTKEY,
         'tools',
@@ -24,7 +24,7 @@ if (TYPO3_MODE === 'BE') {
             'labels' => 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_backend.xml',
         )
     );
-}
+}*/
 
 t3lib_div::loadTCA('fe_users');
 $feUserColumns = array(
