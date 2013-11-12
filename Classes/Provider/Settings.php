019d9250807a6235c84516821b4444cb027daf12
André L. <andre@programmerq.eu>
2013-11-12 11:19:05 +0100

    Prevent Exception if boolean value is passed
    In some cases, a Boolean value is passed, which array-merge can not handle.

--- Classes/Provider/Settings.php
+++ Classes/Provider/Settings.php
@@ -177,7 +177,9 @@ class Tx_T3chimp_Provider_Settings {
             $this->settings = array_merge($this->settings, $this->configurationManager->getConfiguration(Tx_Extbase_Configuration_ConfigurationManagerInterface::CONFIGURATION_TYPE_FRAMEWORK));
             $global = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$this->extKey]);
             $global = $this->cleanSettingKeys($global);
-            self::$settingsCache[$this->extKey] = array_merge($this->settings, $global);
+            if(is_array($global)) {
+               self::$settingsCache[$this->extKey] = array_merge($this->settings, $global); 
+            }
         }
 
         $this->settings = self::$settingsCache[$this->extKey];
