db426b3c224dcc9551026f8d3f6acd3f4a661b21
Mato Ilic <info@matoilic.ch>
2014-04-22 17:11:40 +0200

    fixes #17 curl error handling improved

--- Classes/MailChimp/MailChimpApi.php
+++ Classes/MailChimp/MailChimpApi.php
@@ -18,6 +18,88 @@ use MatoIlic\T3Chimp\MailChimp\MailChimpApi\Users;
 use MatoIlic\T3Chimp\MailChimp\MailChimpApi\Vip;
 
 class MailChimpApi {
+    const CURL_MAX_REDIRECTS = 10;
+
+    private static $curlCodes = array(
+        'CURL_OK',
+        'CURLE_UNSUPPORTED_PROTOCOL',
+        'CURLE_FAILED_INIT',
+        'CURLE_URL_MALFORMAT',
+        'CURLE_URL_MALFORMAT_USER',
+        'CURLE_COULDNT_RESOLVE_PROXY',
+        'CURLE_COULDNT_RESOLVE_HOST',
+        'CURLE_COULDNT_CONNECT',
+        'CURLE_FTP_WEIRD_SERVER_REPLY',
+        'CURLE_REMOTE_ACCESS_DENIED',
+        'CURLE_FTP_WEIRD_PASS_REPLY',
+        'CURLE_FTP_WEIRD_PASV_REPLY',
+        'CURLE_FTP_WEIRD_227_FORMAT',
+        'CURLE_FTP_CANT_GET_HOST',
+        'CURLE_FTP_COULDNT_SET_TYPE',
+        'CURLE_PARTIAL_FILE',
+        'CURLE_FTP_COULDNT_RETR_FILE',
+        'CURLE_QUOTE_ERROR',
+        'CURLE_HTTP_RETURNED_ERROR',
+        'CURLE_WRITE_ERROR',
+        'CURLE_UPLOAD_FAILED',
+        'CURLE_READ_ERROR',
+        'CURLE_OUT_OF_MEMORY',
+        'CURLE_OPERATION_TIMEDOUT',
+        'CURLE_FTP_PORT_FAILED',
+        'CURLE_FTP_COULDNT_USE_REST',
+        'CURLE_RANGE_ERROR',
+        'CURLE_HTTP_POST_ERROR',
+        'CURLE_SSL_CONNECT_ERROR',
+        'CURLE_BAD_DOWNLOAD_RESUME',
+        'CURLE_FILE_COULDNT_READ_FILE',
+        'CURLE_LDAP_CANNOT_BIND',
+        'CURLE_LDAP_SEARCH_FAILED',
+        'CURLE_FUNCTION_NOT_FOUND',
+        'CURLE_ABORTED_BY_CALLBACK',
+        'CURLE_BAD_FUNCTION_ARGUMENT',
+        'CURLE_INTERFACE_FAILED',
+        'CURLE_TOO_MANY_REDIRECTS',
+        'CURLE_UNKNOWN_TELNET_OPTION',
+        'CURLE_TELNET_OPTION_SYNTAX',
+        'CURLE_PEER_FAILED_VERIFICATION',
+        'CURLE_GOT_NOTHING',
+        'CURLE_SSL_ENGINE_NOTFOUND',
+        'CURLE_SSL_ENGINE_SETFAILED',
+        'CURLE_SEND_ERROR',
+        'CURLE_RECV_ERROR',
+        'CURLE_SSL_CERTPROBLEM',
+        'CURLE_SSL_CIPHER',
+        'CURLE_SSL_CACERT',
+        'CURLE_BAD_CONTENT_ENCODING',
+        'CURLE_LDAP_INVALID_URL',
+        'CURLE_FILESIZE_EXCEEDED',
+        'CURLE_USE_SSL_FAILED',
+        'CURLE_SEND_FAIL_REWIND',
+        'CURLE_SSL_ENGINE_INITFAILED',
+        'CURLE_LOGIN_DENIED',
+        'CURLE_TFTP_NOTFOUND',
+        'CURLE_TFTP_PERM',
+        'CURLE_REMOTE_DISK_FULL',
+        'CURLE_TFTP_ILLEGAL',
+        'CURLE_TFTP_UNKNOWNID',
+        'CURLE_REMOTE_FILE_EXISTS',
+        'CURLE_TFTP_NOSUCHUSER',
+        'CURLE_CONV_FAILED',
+        'CURLE_CONV_REQD',
+        'CURLE_SSL_CACERT_BADFILE',
+        'CURLE_REMOTE_FILE_NOT_FOUND',
+        'CURLE_SSH',
+        'CURLE_SSL_SHUTDOWN_FAILED',
+        'CURLE_AGAIN',
+        'CURLE_SSL_CRL_BADFILE',
+        'CURLE_SSL_ISSUER_ERROR',
+        'CURLE_FTP_PRET_FAILED',
+        'CURLE_FTP_PRET_FAILED',
+        'CURLE_RTSP_CSEQ_ERROR',
+        'CURLE_RTSP_SESSION_ERROR',
+        'CURLE_FTP_BAD_FILE_LIST',
+        'CURLE_CHUNK_FAILED'
+    );
 
     /**
      * Placeholder attribute for Folders class
@@ -324,7 +406,7 @@ class MailChimpApi {
             $response_body = curl_exec($ch);
         } else {
             curl_setopt($ch, CURLOPT_FOLLOWLOCATION, false);
-            $redirectsLeft = 5;
+            $redirectsLeft = self::CURL_MAX_REDIRECTS;
 
             $finalUrl = curl_getinfo($ch, CURLINFO_EFFECTIVE_URL);
             $ch2 = curl_copy_handle($ch);
@@ -336,13 +418,16 @@ class MailChimpApi {
             do {
                 curl_setopt($ch2, CURLOPT_URL, $finalUrl);
                 $header = curl_exec($ch2);
-                if (curl_errno($ch2)) {
-                    $code = 0;
+                if ($errorCode = curl_errno($ch2)) {
+                    $error = self::$curlCodes[$errorCode];
+                    trigger_error('A cURL error occurred when trying to call the MailChimp API: ' . $error, E_USER_ERROR);
                 } else {
                     $code = curl_getinfo($ch2, CURLINFO_HTTP_CODE);
-                    if ($code == 301 || $code == 302) {
+                    if ($code >= 300 && $code <= 399) {
                         preg_match('/Location:(.*?)\n/', $header, $matches);
                         $finalUrl = trim(array_pop($matches));
+                    } else if($code >= 400 && $code <= 599) {
+                        trigger_error('A network error occurred. Could not reach the MailChimp API endpoint.', E_USER_ERROR);
                     } else {
                         $code = 0;
                     }
